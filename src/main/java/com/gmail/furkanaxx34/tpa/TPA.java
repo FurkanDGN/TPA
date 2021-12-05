c340db16f57ed44d663d79a87f4ea357f3f398f3
FurkanDGN <furkanaxx34@gmail.com>
2021-12-05 03:07:00 +0300

    initial commit

---
+++ src/main/java/com/gmail/furkanaxx34/tpa/TPA.java
@@ -0,0 +1,16 @@
+package com.gmail.furkanaxx34.tpa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
